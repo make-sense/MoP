2a22e7840538a75be3b4eb8ed79b4d16cc0dd4ad
Donghoon Lee <info@makesense.cc>
2015-05-18 16:48:44 +0900

    Add Remote Controller

---
+++ Assets/Scripts/ProgramController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProgramController : MonoBehaviour {
+
+	PhotonView pv;
+
+	void Awake () {
+		pv = GetComponent<PhotonView> ();
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ArrowUp () {
+		pv.RPC ("Move", PhotonTargets.All, 1f, 0f);
+	}
+
+	public void ArrowDown () {
+		pv.RPC ("Move", PhotonTargets.All, 0f, 0f);
+	}
+}
