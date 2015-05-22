f3509e06d92eb1589d31136c58d3e00b22cd1c20
Donghoon Lee <info@makesense.cc>
2015-05-22 15:01:33 +0900

    Change RPC call function

--- Assets/Scripts/ProgramController.cs
+++ Assets/Scripts/ProgramController.cs
@@ -20,18 +20,22 @@ public class ProgramController : MonoBehaviour {
 	}
 
 	public void ArrowUp () {
+		Debug.Log ("[ProgramController:ArrowUp] 0.7f, 0ff");
 		pv.RPC ("Move", PhotonTargets.All, 0.7f, 0f);
 	}
 	
 	public void ArrowDown () {
+		Debug.Log ("[ProgramController:ArrowDown] 0f, 0.9f");
 		pv.RPC ("Move", PhotonTargets.All, 0f, 0f);
 	}
 	
 	public void ArrowLeft () {
+		Debug.Log ("[ProgramController:ArrowLeft] 0f, -0.9f");
 		pv.RPC ("Move", PhotonTargets.All, 0f, -0.9f);
 	}
 	
 	public void ArrowRight () {
+		Debug.Log ("[ProgramController:ArrowRight] 0f, 0.9f");
 		pv.RPC ("Move", PhotonTargets.All, 0f, 0.9f);
 	}
 
@@ -40,6 +44,6 @@ public class ProgramController : MonoBehaviour {
 		UILabel labelValue = UISlider.current.transform.FindChild ("LabelValue").GetComponentInChildren<UILabel> ();
 		labelValue.text = ((int)(UISlider.current.value * 300)).ToString () + " deg";
 		Debug.Log ("[ProgramController:SetAngle] ID: " + labelID.text + ", value: " + labelValue.text);
-		pv.RPC ("SetAngle", PhotonTargets.All, 1, UISlider.current.value * 300);
+		pv.RPC ("SetAngle", PhotonTargets.All, System.Convert.ToInt32(labelID.text), (int)(UISlider.current.value * 300));
 	}
 }
