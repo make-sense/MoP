63b0a291a9f7952727b3d0af7a5fdee73d0d7eb6
Donghoon Lee <movehoon@gmail.com>
2015-07-03 20:23:55 +0900

    Making setting panel ~2

---
+++ MoP/Assets/Scripts/SettingManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingManager : MonoBehaviour {
+
+	public GameObject SettingPanel;
+
+	void Start () {
+		SettingPanel.SetActive (false);
+	}
+
+	public void PanelToggle () {
+		SettingPanel.SetActive (!SettingPanel.activeSelf);
+	}
+	public void PanelHide () {
+		SettingPanel.SetActive (false);
+	}
+}
