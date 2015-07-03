ddd492431a4115ed9594e3b9cbc9d0be7e7c727b
Donghoon Lee <movehoon@gmail.com>
2015-07-03 17:16:59 +0900

    Changing network on robot

---
+++ MoP/Assets/Scripts/NetworkServer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkServer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Network.InitializeServer (1, 30000, false);
+		Debug.Log ("NetworkInitializeServer");
+	}
+	
+	void OnPlayerConnected (NetworkPlayer player) {
+		Debug.Log ("OnPlayerConnected");
+	}
+}
